505e0be9a83e72fd28367becd29d3df9105844a9
GodsonFlinto <153415803+GodsonFlinto@users.noreply.github.com>
2025-10-14 22:36:25 +0530

    Created api for reviews and handle the get, create and delete method for it and checked in postman successfully

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct, createReview, getReviews, deleteReview } = require('../controllers/productController')
 const router = express.Router()
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/authenticate')
 
@@ -7,6 +7,11 @@ router.route('/products').get(isAuthenticatedUser, getProducts)
 router.route('/product/:id').get(getSingleProduct)
 router.route('/product/:id').put(updateProduct)
                             .delete(deleteProduct)
+
+router.route('/review').put(isAuthenticatedUser, createReview)
+router.route('/reviews').get(getReviews)
+router.route('/review').delete(deleteReview)
+
 
 //Admin Routes
 router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct)
